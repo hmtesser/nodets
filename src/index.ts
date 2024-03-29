f1bc2fe74a08cbbc417cfeeb89bdc7a87efa5130
hmtesser <hmtesser@gmail.com>
2024-03-28 21:22:23 -0300

    feat: add CreateUsersController

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { config } from "dotenv";
 import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
 import { GetUsersController } from "./controllers/get-users/get-users";
 import { MongoClient } from "./database/mongo";
+import { CreateUserController } from "./controllers/create-user/create-user";
+import { MongoCreateUser } from "./repositories/create-user/mongo-create-user";
 
 config();
 
@@ -25,6 +27,20 @@ const main = async () => {
     const getUsersController = new GetUsersController(mongoGetUsersRepository);
 
     const { body, statusCode } = await getUsersController.handle();
+
+    res.send(body).status(statusCode);
+  });
+
+  app.post("/users", async (req, res) => {
+    const mongoCreateUserRepository = new MongoCreateUser();
+
+    const createUserController = new CreateUserController(
+      mongoCreateUserRepository
+    );
+
+    const { body, statusCode } = await createUserController.handle({
+      body: req.body,
+    });
 
     res.send(body).status(statusCode);
   });
